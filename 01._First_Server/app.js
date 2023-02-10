ba48472beb4acbf49c83ac0fab3b85787e52981f
Thecoder0012 <mardineliassi57@gmail.com>
2023-02-10 11:30:05 +0100

    Added additional routes and txt file

--- 01._First_Server/app.js
+++ 01._First_Server/app.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const app = express();
 
-app.get("/",function(req,res){
-    res.send("hello")
+// we use json because every language has a interpreter that understand json. Object notation of javascript.
+app.get("/",(req,res) => {
+    res.send({message: "Our first route"});
+});
+
+let bicycleSpins = 0;
+app.get("/bicycle",(req,res) => {
+    bicycleSpins += 1;
+    res.send({message: `Spinning the bicycle ${bicycleSpins}`});
+});
+
+let kick = 0;
+app.get("/dinosaur",(req,res) => {
+    kick += 1;
+    res.send({message: `Kicking the dinosaur: ${kick}: OW OW OW`});
+});
+
+app.get("/about",(req,res) => {
+    res.send(`<h1> Hello </h1>`);
+});
+
+
+// for query strings (http://localhost:8080/bat?adjective=boring)
+app.get("/bat",(req,res) => {
+    console.log(req.query.adjective);
+    res.send({message: `The bat is ${req.query.adjective}`})
+});
+
+app.get("/bottle/:bottleSize",(req,res) => {
+    res.send({message: `The Bottle is ${req.params.bottleSize}`})
 });
 
 app.listen(8080);
