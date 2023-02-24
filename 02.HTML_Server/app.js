945e53b405440e51e4938011c16a744d6a03594e
Thecoder0012 <mardineliassi57@gmail.com>
2023-02-24 11:10:51 +0100

    vercel

--- 02.HTML_Server/app.js
+++ 02.HTML_Server/app.js
@@ -1,16 +1,46 @@
+/* eslint-disable semi */
 const express = require('express');
 const app = express();
 
+app.use(express.static('public')); // express has a static folder named public
 
-app.get("/", (req,res) => {
-    res.sendFile(__dirname+"/index.html")
+const tanks = [
+  {name: "Leopard", age: 50},
+  {name: "Tiger"}  
+];
+
+let visitorCount = 0;
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/frontpage/index.html`);
 });
 
+app.get('/tanks', (req, res) => {
+  res.sendFile(__dirname + '/public/tanks/tanks.html');
+});
+
+app.get('/api/tanks', (req,res) => {
+  res.send({data: tanks})
+})
+
+app.get("/api/visitors", (req, res) => {
+  res.send({ data: visitorCount });
+});
+
+app.put("/api/visitors", (req, res) => {
+  res.send({ data: ++visitorCount }); // prefix updates eargerly.
+});
+
+app.get("/visitors", (req, res) => {
+  res.sendFile(__dirname + "/public/visitors/visitors.html");
+});
+
+
 const PORT = 8080;
-app.listen(PORT,(error) => {
-    if(error){
-        console.log(error);
-        return;
-    }
-    console.log("Port started on",PORT)
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log('Port started on', PORT);
 });
