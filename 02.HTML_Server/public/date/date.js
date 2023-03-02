887e56e68442688a36a29e0572df43e5565e0a95
Thecoder0012 <mardineliassi57@gmail.com>
2023-03-02 23:29:08 +0100

    change for vercel

--- 02.HTML_Server/public/date/date.js
+++ 02.HTML_Server/public/date/date.js
@@ -1,25 +1,25 @@
-const textcont = document.querySelector(".text-container");
-let text;
-function getText() {
-  let dateValue = document.querySelector(".date-input").value;
-  let dateObj = new Date(dateValue);
-  let dateFormatted = new Date(dateValue).toLocaleDateString("dk");
-  let values = dateFormatted.split(".");
-  let year = parseInt(values[2]);
+// const textcont = document.querySelector(".text-container");
+// let text;
+// function getText() {
+//   let dateValue = document.querySelector(".date-input").value;
+//   let dateObj = new Date(dateValue);
+//   let dateFormatted = new Date(dateValue).toLocaleDateString("dk");
+//   let values = dateFormatted.split(".");
+//   let year = parseInt(values[2]);
 
-  if (
-    (year % 4 === 0 && year % 100 !== 0) ||
-    year % 400 == 0 ||
-    (year % 100 === 0 && year % 400 !== 0)
-  ) {
-    text = document.createElement("h1");
-    text.innerText = `${dateObj.getFullYear()} is a leap year`;
-    textcont.appendChild(text);
-    return;
-  } else {
-    text = document.createElement("h1");
-    text.innerText = `${dateObj.getFullYear()} is not a leap year`;
-    textcont.appendChild(text);
-    return;
-  }
-}
+//   if (
+//     (year % 4 === 0 && year % 100 !== 0) ||
+//     year % 400 == 0 ||
+//     (year % 100 === 0 && year % 400 !== 0)
+//   ) {
+//     text = document.createElement("h1");
+//     text.innerText = `${dateObj.getFullYear()} is a leap year`;
+//     textcont.appendChild(text);
+//     return;
+//   } else {
+//     text = document.createElement("h1");
+//     text.innerText = `${dateObj.getFullYear()} is not a leap year`;
+//     textcont.appendChild(text);
+//     return;
+//   }
+// }
