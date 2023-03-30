6977c27b90beb589c1f90e712a2746266cd6c5e5
Thecoder0012 <mardineliassi57@gmail.com>
2023-03-30 12:02:51 +0200

    upper

--- 03._Upper/public/pages/quests/quests.js
+++ 03._Upper/public/pages/quests/quests.js
@@ -3,4 +3,4 @@ fetch("https://www.boredapi.com/api/activity")
   .then((result) => {
     const quest = document.getElementById("quest"); // define const inside fetch scope, so it wont exist forever if defined at the top.
     quest.innerText = result.activity; // innerHtml enables crosssite scripting
-});
+  });
